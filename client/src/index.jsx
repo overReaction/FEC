5197d8259b1bc077ef1179f7ddb3436e1250b6b0
Jenna Groth <jenna.groth18@alumni.colostate.edu>
2021-04-15 16:19:24 -0600

    "Initialized index.jsx, index.js, index.html"

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,3 +2,27 @@ import Overview from './components/overview.jsx';
 import RatingsReviews from './components/ratingsReviews.jsx';
 import QuestionsAnswers from './components/questionsAnswers.jsx';
 import RelatedItemsComparison from './components/relatedItemsComparison.jsx';
+import React, { useState, useEffect } from 'react';
+
+
+var App = () => {
+  //declaring a new state variable, called stateVariable, and corresponding set function
+  const [stateVariable, setStateVariable] = useState('initial value');
+
+  //Use Effect is similiar to component did mount
+  useEffect(() => {
+    document.title = `Let's OverReact!`;
+  });
+
+
+  return (
+    <div>
+      <Overview state={stateVariable}/>
+      <RatingsReviews setState={setStateVariable}/>
+      <QuestionsAnswers />
+      <RelatedItemsComparison />
+    </div>
+  );
+};
+
+export default App;
