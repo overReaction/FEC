04b16e46edd91ee432c06830af7f2415154ff794
Matt Salmons <mattcsalmons@gmail.com>
2021-04-15 17:26:58 -0600

    Succesfully retrieve API data

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  API_KEY: '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'
+  API_KEY: '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'
 };
--- helpers/atelier.js
+++ helpers/atelier.js
@@ -1,19 +1,15 @@
 const axios = require('axios');
 const config = require('../config');
-
-
 const API_URL = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-bld';
 
-
 const getAllProducts = (callback) => {
-  let options = {
+  axios.get(`${API_URL}/products`, {
     headers: {
-      Authorization: `${config.API_KEY}`
+      Authorization: config.API_KEY
     }
-  };
-  axios.get(`${API_URL}/products`, options.headers)
+  })
     .then(response => {
-      callback(response.data);
+      callback(null, response.data);
     })
     .catch(error => {
       console.log('Err getting all products from Atelier', error);
