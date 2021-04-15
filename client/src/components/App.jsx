90865519d0617668bd38a5eb9f22b0d1ad9a54fc
Jenna Groth <jenna.groth18@alumni.colostate.edu>
2021-04-15 16:37:26 -0600

    "Renamed index.jsx to app.jsx, renamed index.js, adjusted file paths"

---
+++ client/src/components/App.jsx
@@ -0,0 +1,28 @@
+import Overview from './overview.jsx';
+import RatingsReviews from './ratingsReviews.jsx';
+import QuestionsAnswers from './questionsAnswers.jsx';
+import RelatedItemsComparison from './relatedItemsComparison.jsx';
+import React, { useState, useEffect } from 'react';
+
+
+var App = () => {
+  //declaring a new state variable, called stateVariable, and corresponding set function
+  const [stateVariable, setStateVariable] = useState('initial value');
+
+  //Use Effect is similiar to component did mount
+  useEffect(() => {
+    document.title = `Let's OverReact!`;
+  });
+
+
+  return (
+    <div>
+      <Overview state={stateVariable}/>
+      <RatingsReviews setState={setStateVariable}/>
+      <QuestionsAnswers />
+      <RelatedItemsComparison />
+    </div>
+  );
+};
+
+export default App;
