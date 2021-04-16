f28d4e50a748ba248099a7a5c4c9723729b7a587
Matt Salmons <mattcsalmons@gmail.com>
2021-04-16 16:06:01 -0600

    q.toUpperCase()

--- client/src/components/questionsAnswers/questionsAnswers.test.jsx
+++ client/src/components/questionsAnswers/questionsAnswers.test.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { render, screen } from '@testing-library/react'; //Allows artificial rendering
 import userEvent from '@testing-library/user-event'; //Allows triggering of user events. Not demo'd on this page.
 import '@testing-library/jest-dom'; //Provides a set of custom jest matchers that you can use to extend jest. These will make your tests more declarative, clear to read and to maintain.
-import QuestionsAnswers from './questionsAnswers.jsx';
+import QuestionsAnswers from './QuestionsAnswers.jsx';
 
 /* Some example templates for testing are provided below. In general, you will
 1)render the component (see examples below)
