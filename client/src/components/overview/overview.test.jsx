99948b918a24b76d7abc9c86392058ec7f26cc6e
Kayla Elortondo <kaylaelortondo@Kaylas-Mac-mini.local>
2021-04-16 16:50:58 -0600

    Wrote tests for the product information component

--- client/src/components/overview/overview.test.jsx
+++ client/src/components/overview/overview.test.jsx
@@ -10,10 +10,82 @@ import Overview from './overview.jsx';
 3)optionally insert user events to manipulate elements: https://testing-library.com/docs/ecosystem-user-event
 4)test assertions about the component: https://github.com/testing-library/jest-dom */
 
+beforeAll(() => {
+  render(<Overview />);
+});
 
 describe('Overview Widget', () => {
-  test('The Overview Widget should render to the screen', () => {
-    render(<Overview/>);
-    expect(screen.getByTestId('overview')).toHaveTextContent('Overview widget placeholder');
+  test('Should render to the screen', () => {
+    expect(screen.getByTestId('overview')).toBeInTheDocument();
+  });
+
+  xtest('Should have an image gallery', () => {
+    expect(screen.getByTestId('gallery')).toBeInTheDocument();
+  });
+
+  xtest('Should have a product information section', () => {
+    expect(screen.getByTestId('product-info')).toBeInTheDocument();
+  });
+
+  xtest('Should have a style selector', () => {
+    expect(screen.getByTestId('style-selector')).toBeInTheDocument();
+  });
+
+  xtest('Should have an add to cart section', () => {
+    expect(screen.getByTestId('add-to-cart')).toBeInTheDocument();
+  });
+});
+
+describe('Product Information component', () => {
+  xtest('Should have a star rating component', () => {
+    expect(screen.getByTestId('star-rating')).toBeInTheDocument();
+  });
+
+  xtest('The star rating should contain a total of 5 stars', () => {
+
+  });
+
+  xtest('The star rating should be representative up to a quarter of a review point', () => {
+
+  });
+
+  xtest('There should be a link next to the star rating stating "Read all <#> reviews', () => {
+
+  });
+
+  xtest('The read all reviews link should scroll the page to Ratings & Reviews section when clicked', () => {
+
+  });
+
+  xtest('The entire star rating section should be hidden if the product has no reviews', () => {
+
+  });
+
+  xtest('Should display a product category', () => {
+
+  });
+
+  xtest('Should display a product title', () => {
+
+  });
+
+  xtest('Should display a price which is derived from the initial default selected style', () => {
+
+  });
+
+  xtest('The price should update based on a style being selected', () => {
+
+  });
+
+  xtest('If an item is on sale, the sale price should appear in red followed by the struckthrough original price', () => {
+
+  });
+
+  xtest('Should show a product overview section if available for that item, and not show it if unavailable', () => {
+
+  });
+
+  xtest('Should have share buttons for Facebook, Twitter, and Pinterest', () => {
+
   });
 });
