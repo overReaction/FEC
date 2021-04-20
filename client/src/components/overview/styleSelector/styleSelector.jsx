29f92375c9b42e181be3e6ba454378ab0f96d948
Kayla Elortondo <kaylaelortondo@Kaylas-Mac-mini.local>
2021-04-20 09:17:09 -0600

    Rendering style thumbnails

--- client/src/components/overview/styleSelector/styleSelector.jsx
+++ client/src/components/overview/styleSelector/styleSelector.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import Grid from '@material-ui/core/Grid';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import { fetchStyleInfo, selectStyle } from '../overviewSlice.js';
@@ -16,9 +17,22 @@ const StyleSelector = (props) => {
   }, [productId]);
 
   if (currentStyle) {
-    return (<div data-testid="style-selector"> Select Style Placeholder {currentStyle.name} </div>);
+    return (
+      <div data-testid="style-selector">Style: {currentStyle.name}
+        <GridList cellHeight={75} cols={4}>
+          {allStyles.map((style, index) => {
+            return (
+              <GridListTile xs={3} key={style.style_id} cols={1}>
+                <img src={style.photos[1].thumbnail_url} />
+              </GridListTile>
+            );
+          }
+          )}
+        </GridList>
+      </div>
+    );
   } else {
-    return (<div data-testid="style-selector"> Select Style Placeholder </div>);
+    return (<div data-testid="style-selector"> Loading </div>);
   }
 };
 
