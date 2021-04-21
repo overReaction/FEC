17df390a9f8c7ba5d5492a8affa436d116d954b2
Kayla Elortondo <kaylaelortondo@Kaylas-Mac-mini.local>
2021-04-21 14:59:52 -0600

    Fixed the bug for the wrong image being displayed in the gallery when it had no photo urls supplied

--- client/src/components/overview/imageGallery/galleryNav.jsx
+++ client/src/components/overview/imageGallery/galleryNav.jsx
@@ -77,6 +77,10 @@ const GalleryNav = (props) => {
               url = photo.thumbnail_url;
             } else {
               url = "https://images.unsplash.com/photo-1599508704512-2f19efd1e35f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=675&q=80";
+              dispatch(setCurrentPhoto({
+                thumbnail_url: "https://images.unsplash.com/photo-1599508704512-2f19efd1e35f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=675&q=80",
+                url: "https://images.unsplash.com/photo-1599508704512-2f19efd1e35f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=675&q=80"
+              }));
             }
             return (
               <Step
--- client/src/components/overview/imageGallery/imageGallerySlice.js
+++ client/src/components/overview/imageGallery/imageGallerySlice.js
@@ -33,6 +33,13 @@ export const gallerySlice = createSlice({
     },
     setStylePhotos: (state, action) => {
       state.allStylePhotos = action.payload;
+      console.log(action.payload);
+      if (action.payload.length === 0) {
+        state.currentPhoto = {
+          thumbnail_url: "https://images.unsplash.com/photo-1599508704512-2f19efd1e35f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=675&q=80",
+          url: "https://images.unsplash.com/photo-1599508704512-2f19efd1e35f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=675&q=80"
+        };
+      }
     }
   }
 });
