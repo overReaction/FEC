f5e488b1a42a16f959fafa33fb48ac22aeb145ad
Matt Salmons <mattcsalmons@gmail.com>
2021-04-23 15:21:50 -0600

    added styling to addQmodal

--- client/src/components/questionsAnswers/AddQuestion.jsx
+++ client/src/components/questionsAnswers/AddQuestion.jsx
@@ -1,27 +1,32 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Button } from '@material-ui/core/';
+import { ButtonGroup } from '@material-ui/core/';
 import Modal from "@material-ui/core/Modal";
 
-function rand () {
-  return Math.round(Math.random() * 20) - 10;
-}
+// function rand () {
+//   return Math.round(Math.random() * 20) - 10;
+// }
 
 function getModalStyle () {
-  const top = 50 + rand();
-  const left = 50 + rand();
+  // const top = 50 + rand();
+  // const left = 50 + rand();
 
   return {
-    top: `${top}%`,
-    left: `${left}%`,
-    transform: `translate(-${top}%, -${left}%)`
+    display: 'flex',
+    flexDirection: 'column',
+    textAlign: 'center',
+    top: '20%',
+    left: '35%',
+    width: '20%'
+    // transform: `translate(-${top}%, -${left}%)`
   };
 }
 
 const useStyles = makeStyles((theme) => ({
   paper: {
     position: "absolute",
-    width: 400,
+    width: 600,
     backgroundColor: theme.palette.background.paper,
     border: "2px solid #000",
     boxShadow: theme.shadows[5],
@@ -45,10 +50,21 @@ export default function AddQModal () {
 
   const body = (
     <div style={modalStyle} className={classes.paper}>
-      <h2 id="simple-modal-title">Ask A Question</h2>
-      <p id="simple-modal-description">
-        Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-      </p>
+      <h2 id="ask-a-question-modal">Ask A Question</h2>
+      <p>(all fields required)</p>
+      <form>
+        Your Nickname: <input name="nickname"/>
+        <br/>
+        Your Email: <input name="email"/>
+        <br/>
+        Your Question: <input style={{ height: 200, width: 200 }} name="question"/>
+        <br/>
+        <br/>
+        <ButtonGroup>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button>Submit</Button>
+        </ButtonGroup>
+      </form>
     </div>
   );
 
