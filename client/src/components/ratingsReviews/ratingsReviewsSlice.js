7c836a7b4bc66e870dcf6549709e348ee26a2903
Kayla Elortondo <kaylaelortondo@Kaylas-Mac-mini.local>
2021-04-26 15:18:00 -0600

    Added functionality for reporting reviews

--- client/src/components/ratingsReviews/ratingsReviewsSlice.js
+++ client/src/components/ratingsReviews/ratingsReviewsSlice.js
@@ -9,26 +9,42 @@ export const markHelpful = createAsyncThunk(
   }
 );
 
+export const reportReview = createAsyncThunk(
+  'reviews/report',
+  async (reviewId, thunkAPI) => {
+    await axios.put(`/api/?endpoint=reviews/${reviewId}/report`);
+    return reviewId;
+  }
+);
+
 export const reviewSlice = createSlice({
   name: 'reviews',
   initialState: {
     reviewsCount: 2,
-    markedHelpful: []
+    markedHelpful: [],
+    markedNotHelpful: [],
+    reported: []
   },
   reducers: {
     loadReviews: (state, action) => {
       state.reviewsCount += 2;
+    },
+    markNonHelpful: (state, action) => {
+      state.markedNotHelpful.push(action.payload);
     }
   },
   extraReducers: {
     [markHelpful.fulfilled]: (state, action) => {
       state.markedHelpful.push(action.payload);
+    },
+    [reportReview.fulfilled]: (state, action) => {
+      state.reported.push(action.payload);
     }
   }
 });
 
 //Action creators are generated for each reducer function. Add multiple like so { reducer1, reducer2, ...}
-export const { loadReviews } = reviewSlice.actions;
+export const { loadReviews, markNonHelpful } = reviewSlice.actions;
 
 //Makes the reducers defined above available to the store
 export default reviewSlice.reducer;
