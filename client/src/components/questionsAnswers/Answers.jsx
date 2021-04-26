5803d2dc6577fe501e97ac8a14081994a5824b08
Matt Salmons <mattcsalmons@gmail.com>
2021-04-26 11:03:31 -0600

    fixed search bug, now new answers don't render on submit...

--- client/src/components/questionsAnswers/Answers.jsx
+++ client/src/components/questionsAnswers/Answers.jsx
@@ -1,14 +1,17 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React, { useState } from 'react';
 import Answer from './Answer.jsx';
 
 
 const Answers = (props) => {
-  const firstTwoAnswers = props.answers[props.index].slice(0, 2);
+  // const answers = props.answers[props.index];
+  const [answers, setAnswers] = useState(props.answers[props.index]);
+  // console.log(firstTwoAnswers);
+  console.log('ANSWERS:', answers);
 
   return (
     <div style={{ marginLeft: 10 }}>
-      {firstTwoAnswers.map((answer, index) => {
+      {answers.map((answer, index) => {
         if (index < 2) {
           return <Answer key={answer.id} answer={answer} index={index}/>;
         }
